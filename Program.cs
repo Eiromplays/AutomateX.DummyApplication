2a0f7ec738f510f4ca93296e399d975173e1a165
Eiromplays <eiromplays@protonmail.com>
2024-06-04 14:09:27 +0200

    Fjern feil

--- Program.cs
+++ Program.cs
@@ -23,7 +23,7 @@ var summaries = new[]
 
 app.MapGet("/weatherforecast", () =>
     {
-        throw new Exception("This is an exception!");
+        //throw new Exception("This is an exception!");
         var forecast = Enumerable.Range(1, 5).Select(index =>
                 new WeatherForecast
                 (
